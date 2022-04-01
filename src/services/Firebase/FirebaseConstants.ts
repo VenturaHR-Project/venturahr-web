a77b57f9188c7d6b4f8059e20d4debb14b60704d
Matheus Evangelista <matheus.evangelista@g.globo>
2022-03-31 23:32:50 -0300

    refactor: update gitignoree

---
+++ src/services/Firebase/FirebaseConstants.ts
@@ -0,0 +1,8 @@
+export class FirebaseConstants {
+    public static apiKey = "AIzaSyD7oZDF-peyICD1pHfUn_o0nWFxPGv6vEQ"
+    public static authDomain = "venturahr-a6fc9.firebaseapp.com"
+    public static projectId = "venturahr-a6fc9"
+    public static storageBucket = "venturahr-a6fc9.appspot.com"
+    public static messagingSenderId = "419242748521"
+    public static appId = "1:419242748521:web:fdb6691b10f71c6b0bd0f8"
+}
